7c783d5c0e2ee5b8003782afc958a014e054f1b0
Alumno Administración <aadmon@utags.edu.mx>
2022-10-18 19:52:01 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function ScrollDown(number) {
+    window.scroll(0, number);
+}
+
+const person = {name:"john", lastname:"Doe", age:50}
+const coppiedPerson = {...person}
+coppiedPerson = "Juan"
+console.log(person)
+
+console.log('object');
